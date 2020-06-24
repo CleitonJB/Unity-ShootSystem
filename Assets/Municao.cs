602862e40ab9920813b30b2bb5e79768e8840f34
Cleiton Braga <cleitonbraga56@gmail.com>
2020-06-24 18:50:36 -0300

    initial files

---
+++ Assets/Municao.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Municao : MonoBehaviour
+{
+    public int velocidade;
+    private Rigidbody corpo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        corpo = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        corpo.velocity = transform.forward * velocidade;
+    }
+}
---
+++ Assets/Weapon.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public GameObject Bala;
+    public Transform Ponta;
+
+    public int ammo;
+    private int ammoInicial;
+    public int ammoReserva;
+
+    public float fireRate;
+    private float canshoot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ammoInicial = ammo;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        canshoot += Time.deltaTime;
+
+        Shoot();
+        Reload();
+    }
+
+    void Shoot(){
+        if(Input.GetMouseButton(0)){
+            if(canshoot >= fireRate && ammo > 0){
+                Instantiate(Bala, Ponta.position, Ponta.rotation);
+                ammo--;
+                canshoot = 0;
+            }
+        }
+    }
+
+    void Reload(){
+        if(Input.GetKeyDown(KeyCode.R) && ammo != ammoInicial){
+            ammoReserva += ammo;
+
+            if(ammoReserva > ammoInicial){
+                ammo -= ammo; 
+                ammo = ammoInicial;
+                ammoReserva -= ammoInicial;
+            }
+            if(ammoReserva == ammoInicial){
+                ammo = ammoReserva;
+                ammoReserva = 0;
+            }
+            if(ammoReserva < ammoInicial){
+                ammo = ammoReserva;
+                ammoReserva = 0;
+            }
+        }
+    }
+}
